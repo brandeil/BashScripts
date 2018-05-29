babe7b30624e153a1f7c7d250719707e78135357
brandeil <glenb73@gmail.com>
2018-05-28 20:21:47 -0400

    allow multiple directories

--- backup.sh
+++ backup.sh
@@ -1,59 +1,66 @@
 #!/bin/bash
 
 # back up a users home directory to /tmp
-if [ -z $1 ]; then
-	user=$(whoami)
-else
-	if [ ! -d "/home/$1" ]; then
-		echo "Requested $1 user home directory doesn't exist."
-		exit 1
+function backup {
+	if [ -z $1 ]; then
+		user=$(whoami)
+	else
+		if [ ! -d "/home/$1" ]; then
+			echo "Requested $1 user home directory doesn't exist."
+			exit 1
+		fi
+		user=$1
 	fi
-	user=$1
-fi
 
-echo "User is ${user}"
+	input=/home/${user}
+	output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-input=/home/${user}
-output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
+	# this function reports the total number of files in the directory
+	function total_files {
+		find $1 -type f | wc -l
+	}
 
-# this function reports the total number of files in the directory
-function total_files {
-	find $1 -type f | wc -l
+	# this function reports the total number of directories
+	function total_directories {
+		find $1 -type d | wc -l
+	}
+
+	function total_archived_directories {
+		tar -tzf $1 | grep /$ | wc -l
+	}
+
+	function total_archived_files {
+		tar -tzf $1 | grep -v /$ | wc -l
+	}
+
+	tar -czf $output $input 2> /dev/null
+	#echo "after tar"
+	src_files=$( total_files ${input} )
+	src_directories=$( total_directories ${input} )
+
+	arch_files=$( total_archived_files ${output} )
+	arch_directories=$( total_archived_directories ${output} )
+
+	echo "################ ${user} ################"
+	echo "Files to be included: ${src_files}"
+	echo "Directories to be included: ${src_directories}"
+	echo "Files archived: ${arch_files}"
+	echo "Directories archived: ${arch_directories}"
+
+	if [ ${src_files} -eq ${arch_files} ]; then
+		echo "Backup of $input completed"
+		echo "Details about the output backup file:"
+		ls -l $output
+	else
+		echo "Backup of $input failed"
+	fi
+
+	#for i in $( ls ); do echo "${i} has $( wc -m < ${i} )"; done
+
 }
 
-# this function reports the total number of directories
-function total_directories {
-	find $1 -type d | wc -l
-}
 
-function total_archived_directories {
-	tar -tzf $1 | grep /$ | wc -l
-}
+for directory in $*; do
+	backup $directory
+done;
 
-function total_archived_files {
-	tar -tzf $1 | grep -v /$ | wc -l
-}
-
-tar -czf $output $input 2> /dev/null
-#echo "after tar"
-src_files=$( total_files ${input} )
-src_directories=$( total_directories ${input} )
-
-arch_files=$( total_archived_files ${output} )
-arch_directories=$( total_archived_directories ${output} )
-echo "after functions"
-
-echo "Files to be included: ${src_files}"
-echo "Directories to be included: ${src_directories}"
-echo "Files archived: ${arch_files}"
-echo "Directories archived: ${arch_directories}"
-
-if [ ${src_files} -eq ${arch_files} ]; then
-	echo "Backup of $input completed"
-	echo "Details about the output backup file:"
-	ls -l $output
-else
-	echo "Backup of $input failed"
-fi
-
-#for i in $( ls ); do echo "${i} has $( wc -m < ${i} )"; done
